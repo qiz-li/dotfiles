a9308ecc4cf651d90336b643e9098e1f1b042d59
qiz-li <47799695+qiz-li@users.noreply.github.com>
2022-02-23 20:32:16 -0800

    ♻️  refactor: Cleaner install script

--- install/install_packages.py
+++ install/install_packages.py
@@ -3,21 +3,24 @@ import platform
 import yaml
 
 
-def install(packages, key=None):
+def print_message(package):
+    print(f"\n----------{'-'*(len(package) + 1)}\nInstalling {package}\n")
+
+
+def install(package, installer):
     """Check for OS and install packages using the appropriate installer.
-    If only certain installers are given, use only those."""
-    for installer, package in packages.items():
-        single = installer == 'all'
-        package = key if not package else package
+       If only certain installers are given, use only those."""
+    if installer == 'pip':
+        print_message(package)
+        os.system(f'python3 -m pip install {package}')
 
-        if installer == 'pip':
-            os.system(f'python3 -m pip install {package}')
+    elif platform.system() == 'Darwin' and installer in ('brew', 'all'):
+        print_message(package)
+        os.system(f'brew install --no-quarantine {package}')
 
-        elif platform.system() == 'Darwin' and (installer == 'brew' or single):
-            os.system(f'brew install --no-quarantine {package}')
-
-        elif platform.system() == 'Linux' and (installer == 'apt' or single):
-            os.system(f'sudo apt install {package}')
+    elif platform.system() == 'Linux' and installer in ('apt', 'all'):
+        print_message(package)
+        os.system(f'sudo apt install {package}')
 
 
 def main():
@@ -28,12 +31,9 @@ def main():
     with open(f'{os.path.dirname(__file__)}/packages.yaml', 'r') as file:
         config = yaml.load(file, Loader=yaml.FullLoader)
 
-    for key, value in config.items():
-        print(f"----------{'-'*(len(key)+1)}\nInstalling {key}\n")
-        if value:
-            install(value, key=key)
-        else:
-            install({'all': key})
+    for package, installer in config.items():
+        install(package, installer if installer else 'all')
+    print()
 
 
 if __name__ == '__main__':
